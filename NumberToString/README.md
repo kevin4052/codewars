de4927eefc2a304ec2a633d7fb177007841d9c9b
Kevin Hernandez <cobra40@gmail.com>
2020-06-27 09:21:14 -0400

    Number toString

---
+++ NumberToString/README.md
@@ -0,0 +1,1 @@
+[`Number toString`](https://www.codewars.com/kata/53934feec44762736c00044b/)
---
+++ NumberToString/script.js
@@ -0,0 +1,9 @@
+// my solution
+var a = 123;
+
+a = a.toString();
+
+// best practice, clever solution
+var a = 123..toString();
+var a = (123).toString();
+var a = 123 + '';
