c5dccdb254bd17be17e0e90238996c748bf479b2
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-08-22 16:11:37 -0400

    refactored sort the odd

--- 6kyu/sortTheOdd/script.js
+++ 6kyu/sortTheOdd/script.js
@@ -1,28 +1,14 @@
 //my solution
 function sortArray(array) {
-    let count = 0;
-    let newArray = [...array];
+  
+  const odd = array.filter(num => num % 2 !== 0).sort((a, b) => a - b); 
+  
+  const sortTheOdd = array.map(num => num % 2 === 0 ? num : odd.shift());
     
-    const odd = newArray.filter((num, index) => {
-      if (num % 2 !== 0){
-        newArray[index] = "*";
-        return num
-      }
-    }).sort((a, b) => a - b); 
-    
-    const sortTheOdd = newArray.map(num => {
-    if (num === "*") {
-        num = odd[count];
-        count++;
-    }
-    return num;
-    });
-    
-    return sortTheOdd
+  return sortTheOdd
 }
-
 // best practice solution
 function sortArray(array) {
-        const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
-        return array.map((x) => x % 2 ? odd.shift() : x);
-    }
+    const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+    return array.map((x) => x % 2 ? odd.shift() : x);
+}
