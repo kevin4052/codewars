dd07295c3f056eba4d38a4e7b734e464e77ad1eb
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-09-19 11:33:26 -0400

    count javascript devs

---
+++ 7kyu/count-javascript-devs/readme.md
@@ -0,0 +1,1 @@
+[` Count the number of JavaScript developers coming from Europe`](https://www.codewars.com/kata/582746fa14b3892727000c4f/javascript)
---
+++ 7kyu/count-javascript-devs/script.js
@@ -0,0 +1,11 @@
+// my solution
+function countDevelopers(list) {  
+    const numberOfDevs = list.filter(dev => dev.continent === 'Europe' && dev.language === 'JavaScript');
+    return numberOfDevs.length;
+}
+
+// best practice
+function countDevelopers(list) {
+    return list.filter(x=>x.continent=='Europe'&&x.language=='JavaScript').length
+  }
+  
