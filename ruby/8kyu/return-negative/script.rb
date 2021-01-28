5dffa66a054b23b36ee9b4ffc46c1fa7e37b7b66
Kevin Hernandez <kevin.hernandez001@gmail.com>
2021-01-28 10:00:51 -0500

    added best practice to return negative

--- ruby/8kyu/return-negative/script.rb
+++ ruby/8kyu/return-negative/script.rb
@@ -1,10 +1,15 @@
 # Return Negative
 
-#my solution
+# -------my solution-------
 def makeNegative(num)
     if num > 0
       return num * -1
     else
       return num
     end
+end
+
+# -------best practice-------
+def makeNegative(num)
+    -num.abs
 end
