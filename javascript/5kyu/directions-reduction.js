5261058cc6bf809562c44ab776a82a2e48edbeaf
kevin4052 <kevin.hernandez001@gmail.com>
2021-02-08 14:43:59 -0500

    directions reduction

---
+++ javascript/5kyu/directions-reduction.js
@@ -0,0 +1,38 @@
+//
+
+// ----------my solution----------
+const dirReduc = (arr) => {
+  const dir = [];
+  let i = 0;
+
+  while (i < arr.length) {
+    if (
+      (arr[i] === "NORTH" && arr[i + 1] === "SOUTH") ||
+      (arr[i] === "SOUTH" && arr[i + 1] === "NORTH") ||
+      (arr[i] === "EAST" && arr[i + 1] === "WEST") ||
+      (arr[i] === "WEST" && arr[i + 1] === "EAST")
+    ) {
+      i++;
+    } else {
+      dir.push(arr[i]);
+    }
+    i++;
+  }
+
+  return dir.length === arr.length ? dir : dirReduc(dir);
+};
+
+// ----------best practice----------
+function dirReduc(plan) {
+  var opposite = {
+    NORTH: "SOUTH",
+    EAST: "WEST",
+    SOUTH: "NORTH",
+    WEST: "EAST",
+  };
+  return plan.reduce(function (dirs, dir) {
+    if (dirs[dirs.length - 1] === opposite[dir]) dirs.pop();
+    else dirs.push(dir);
+    return dirs;
+  }, []);
+}
